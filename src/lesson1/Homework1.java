79a35d9255f67c11dcebd017ec0c45a953835455
Alexey <charushkin.lexa@yandex.ru>
2022-10-27 10:21:02 +0300

    Выполнил внесение изменений

--- src/lesson1/Homework1.java
+++ src/lesson1/Homework1.java
@@ -2,7 +2,7 @@ package lesson1;
 
 public class Homework1 {
     public static void main(String[] args) {
-
+        System.out.println("Внесение изменений");
     }
     private static void task1() {
         System.out.println("Решеие первой задачи");
